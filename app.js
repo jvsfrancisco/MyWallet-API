8cbb398ccea27d0a7f90df1fe68f4dbfc5bdfa82
João Victor <jvictor.franci@gmail.com>
2022-05-07 19:39:13 -0300

    Sign-In

--- app.js
+++ app.js
@@ -5,3 +5,35 @@ import cors from 'cors';
 import {MongoClient} from 'mongodb';
 import bcrypt from 'bcrypt';
 import joi from 'joi';
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("MyWalletDB");
+});
+
+app.post("/sign-in", async (req, res) => {
+  const {email, password} = req.body;
+  try {
+    const user = await db.collection("users").findOne({email});
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.sendStatus(200);
+    } else {
+     res.sendStatus(401);
+     
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+
+app.listen(5000, () => {
+  console.log(chalk.pink('Server is listening on port 5000.'));
+});
